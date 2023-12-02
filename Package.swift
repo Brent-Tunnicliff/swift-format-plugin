fd9c2a047d44144cd79b89ec6ce4e1f74e32aa49
Brent Tunnicliff <brenttunnicliff@gmail.com>
2023-12-02 19:50:45 +1100

    Created plugin for lint.

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-format-plugin",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+        .tvOS(.v17),
+        .watchOS(.v10),
+    ],
+    products: [
+        // Products can be used to vend plugins, making them visible to other packages.
+        .plugin(
+            name: "lint",
+            targets: ["lint"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-format", .upToNextMajor(from: "509.0.0"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .plugin(
+            name: "lint",
+            capability: .buildTool(),
+            dependencies: [
+                .product(name: "swift-format", package: "swift-format")
+            ]
+        ),
+    ]
+)
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # swift-format-plugin
+
 Xcode build plugin for the swift-format package.
+
+Currently only supports linting.
+
+## How to use
+
+Add the package via SRM, then add the `lint` plugin to the targets you want to apply lint to at build time.
+
+```swift
+let package = Package(
+    name: "app",
+    products: [
+        .library(
+            name: "App",
+            targets: ["App"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Brent-Tunnicliff/swift-format-plugin", .upToNextMajor(from: "1.0.0")) // <- Add dependency
+    ],
+    targets: [
+        .target(
+            name: "App",
+            plugins: [
+                .plugin(name: "lint", package: "swift-format-plugin") // <- Add to target
+            ]
+        ),
+        .testTarget(
+            name: "AppTests",
+            dependencies: ["App"],
+            plugins: [
+                .plugin(name: "lint", package: "swift-format-plugin") // <- Add to test target
+            ]
+        ),
+    ]
+)
+```
