d91247794a1f78e15379187387cc0e65068a3f49
Brent Tunnicliff <brent@tunnicliff.dev>
2025-12-12 15:47:56 +1100

    Bumped Swift to 6.1 and removed work arounds to get the directory url.

--- Plugins/LintBuildPlugin/plugin.swift
+++ Plugins/LintBuildPlugin/plugin.swift
@@ -11,7 +11,7 @@ struct LintBuildPlugin: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
         [
             try lint(
-                targetDirectory: target.targetDirectory,
+                targetDirectory: target.directoryURL,
                 tool: context.tool(named: swift)
             )
         ]
@@ -35,21 +35,6 @@ extension LintBuildPlugin: XcodeBuildToolPlugin {
 
 #endif
 
-extension LintBuildPlugin {
-    enum RunError: Error {
-        case unableToGetTargetDirectory(String)
-    }
-}
-
-extension LintBuildPlugin.RunError: CustomStringConvertible {
-    var description: String {
-        switch self {
-        case let .unableToGetTargetDirectory(type):
-            "Unable to get target directory for type \(type)"
-        }
-    }
-}
-
 private extension LintBuildPlugin {
     func lint(targetDirectory: URL, tool: PluginContext.Tool) -> Command {
         let executable = tool.url
@@ -63,29 +48,5 @@ private extension LintBuildPlugin {
 
         let displayName = "Linting the source code: \(executable.relativePath) \(arguments.joined(separator: " "))"
         return .buildCommand(displayName: displayName, executable: executable, arguments: arguments)
-    }
-}
-
-private extension Target {
-    // Swift 6.1 gives us a much nicer solution, so we will switch to that when we can.
-    @available(_PackageDescription, deprecated: 6.1, renamed: "directoryURL")
-    var targetDirectory: URL {
-        get throws {
-            guard let directoryURL = knownTargetDirectory else {
-                throw LintBuildPlugin.RunError.unableToGetTargetDirectory("\(self.self)")
-            }
-
-            return directoryURL
-        }
-    }
-
-    private var knownTargetDirectory: URL? {
-        switch self {
-        case let target as BinaryArtifactTarget: target.directoryURL
-        case let target as SystemLibraryTarget: target.directoryURL
-        case let target as SwiftSourceModuleTarget: target.directoryURL
-        case let target as ClangSourceModuleTarget: target.directoryURL
-        default: nil
-        }
     }
 }
