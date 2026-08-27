9567cd372443b41615ccb1a3dc178944a393db89
Brent Tunnicliff <brent@tunnicliff.dev>
2026-08-27 23:37:28 +1000

    Edited plugin to use a list of files instead of a recursive directory.
    I also passed it in as the inputFiles as I am hoping this will lead to it being skipped if no file changes have happened since last build.

--- Plugins/LintBuildPlugin/plugin.swift
+++ Plugins/LintBuildPlugin/plugin.swift
@@ -9,10 +9,12 @@ struct LintBuildPlugin: BuildToolPlugin {
 
     /// Entry point for creating build commands for targets in Swift packages.
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
-        let tool = try context.tool(named: swift)
-        return [
-            lint(url: target.directoryURL, tool: tool),
-            lint(url: context.package.directoryURL.appending(path: "Package.swift"), tool: tool),
+        [
+            lint(
+                tool: try context.tool(named: swift),
+                files: (target.sourceModule?.sourceFiles.map(\.url) ?? [])
+                    + [context.package.directoryURL.appending(path: "Package.swift")]
+            )
         ]
     }
 }
@@ -21,26 +23,39 @@ struct LintBuildPlugin: BuildToolPlugin {
 import XcodeProjectPlugin
 
 extension LintBuildPlugin: XcodeBuildToolPlugin {
-    // Entry point for creating build commands for targets in Xcode projects.
+    /// Entry point for creating build commands for targets in Xcode projects.
     func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
-        [try lint(url: context.xcodeProject.directoryURL, tool: context.tool(named: swift))]
+        [
+            lint(
+                tool: try context.tool(named: swift),
+                files: target.inputFiles.map(\.url)
+            )
+        ]
     }
 }
 
 #endif
 
 private extension LintBuildPlugin {
-    func lint(url: URL, tool: PluginContext.Tool) -> Command {
+    func lint(
+        tool: PluginContext.Tool,
+        files: [URL]
+    ) -> Command {
         let executable = tool.url
+        let swiftFiles = files.filter { $0.pathExtension == "swift" }
+
         let arguments = [
             "format",
             "lint",
-            url.relativePath,
-            "--recursive",
             "--parallel"
-        ]
+        ] + swiftFiles.map { $0.path(percentEncoded: false) }
 
         let displayName = "Linting the source code: \(executable.relativePath) \(arguments.joined(separator: " "))"
-        return .buildCommand(displayName: displayName, executable: executable, arguments: arguments)
+        return .buildCommand(
+            displayName: displayName,
+            executable: executable,
+            arguments: arguments,
+            inputFiles: swiftFiles
+        )
     }
 }
